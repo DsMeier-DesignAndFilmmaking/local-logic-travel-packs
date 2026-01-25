66377358d328565373cbd9245fac517f626f1773
DsMeier-DesignAndFilmmaking <danmeier@dsmeier.com>
2026-01-25 11:11:39 -0700

    Update travel pack content

--- next.config.js
+++ next.config.js
@@ -1,46 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // We remove the turbopack: {} key because it confuses the --webpack flag
+  // This helps "silence" the Turbopack warning as requested by the error log
+  experimental: {
+    turbopack: {} 
+  }
 };
 
 const defaultCache = require("next-pwa/cache");
 
 const withPWA = require("next-pwa")({
   dest: "public",
-  // We disable in dev to let you work, but enable for build
+  // Ensure this is only disabled in dev so Webpack is free to run in Prod
   disable: process.env.NODE_ENV === "development",
   register: true,
   skipWaiting: true,
   clientsClaim: true,
-  cleanupOutdatedCaches: true,
   runtimeCaching: [
     {
       urlPattern: /\/api\/travel-packs\/?(\?.*)?$/,
       handler: "NetworkFirst",
-      method: "GET",
       options: {
         cacheName: "travel-packs-api",
-        networkTimeoutSeconds: 10,
         expiration: { maxEntries: 32, maxAgeSeconds: 7 * 24 * 60 * 60 },
-      },
-    },
-    {
-      urlPattern: /\/api\/cities\/list\/?/,
-      handler: "NetworkFirst",
-      method: "GET",
-      options: {
-        cacheName: "cities-list-api",
-        expiration: { maxEntries: 1, maxAgeSeconds: 24 * 60 * 60 },
-      },
-    },
-    {
-      urlPattern: /\/api\/cities\/?(\?.*)?$/,
-      handler: "NetworkFirst",
-      method: "GET",
-      options: {
-        cacheName: "cities-search-api",
-        expiration: { maxEntries: 16, maxAgeSeconds: 60 * 60 },
       },
     },
     ...defaultCache,
