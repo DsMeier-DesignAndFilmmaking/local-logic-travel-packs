0cde988329f5a5d00297d62b3e8e78234ff76075
DsMeier-DesignAndFilmmaking <danmeier@dsmeier.com>
2026-01-25 11:36:23 -0700

    Update travel pack content

--- next.config.js
+++ next.config.js
@@ -1,21 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    // This is the "secret sauce" for Next.js 15/16 Webpack compatibility
+    webpackBuildWorker: true, 
+  },
 };
 
+// Ensure you are using the maintained fork we swapped to
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
   skipWaiting: true,
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  swcMinify: true,
-  workboxOptions: {
-    disableDevLogs: true,
-  },
-  // We manually define your travel pack caching here
   runtimeCaching: [
     {
       urlPattern: /\/api\/travel-packs\/?(\?.*)?$/,
