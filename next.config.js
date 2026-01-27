468284fd4d7f1c272b12489eff7fd212664a1df0
DsMeier-DesignAndFilmmaking <danmeier@dsmeier.com>
2026-01-27 09:13:48 -0700

    Update travel pack content

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   reactStrictMode: true,
+  // Explicitly disable Turbopack to use webpack
+  turbopack: {},
   webpack: (config) => {
     delete config.resolve.alias['webpack'];
     return config;
